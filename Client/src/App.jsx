c674dddb7a0d5622aaacd713a4d5e62a2cfebcb4
Yash <ritikverma23@gmail.com>
2025-08-26 17:34:42 +0530

    This adds the main App component, sets up a Navbar, and configures React Router routes for navigation.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import Navbar from './components/Navbar.jsx'
+// Routes is like a container that holds all your routes.
+// Route defines path
+import {Routes, Route} from 'react-router-dom'
+
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
+import SettingsPage from './pages/SettingsPage'
+import ProfilePage from './pages/ProfilePage'
 
 const App = () => {
   return (
-    <div className='text-red-500'>Hello</div>
+    <div >
+      {/* <Navbar/> → Renders the navigation bar on top (always visible). */}
+      <Navbar/>
+      {/* Handles page routing. */}
+      <Routes> 
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/signup' element={<SignUpPage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/settings' element={<SettingsPage/>}/>
+        <Route path='/profile' element={<ProfilePage/>}/>
+      </Routes>
+    </div>
   )
 }
 
