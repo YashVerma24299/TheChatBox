9e20e4e16b3e86e083074719f2741bc92a663245
Yash <ritikverma23@gmail.com>
2025-08-27 23:10:32 +0530

    Added CORS middleware to allow frontend access.

--- Server/src/index.js
+++ Server/src/index.js
@@ -5,12 +5,22 @@ dotenv.config(); // used for env
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import messageRoutes from "./routes/message.route.js";
+import cors from "cors";
 
 const app=express();
 const PORT=process.env.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+
+
+// you’re telling your backend to handle requests coming from a different origin (frontend side).
+app.use(
+  cors({
+    origin: "http://localhost:5173",  // Allow only this frontend
+  credentials: true,                // Allow cookies, tokens, sessions
+  })
+);
 
 app.use("/api/auth", authRoutes); // Handle login,logout,signup route
 app.use("/api/mesages", messageRoutes);
