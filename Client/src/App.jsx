f9a13e4127aa53dc0bebc0a50b7752013d78df09
Yash <ritikverma23@gmail.com>
2025-08-30 23:42:20 +0530

    Added a new SettingsPage featuring a theme selection grid with 32 DaisyUI themes, integrated live chat preview with mock messages.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -10,6 +10,7 @@ import LoginPage from './pages/LoginPage'
 import SettingsPage from './pages/SettingsPage'
 import ProfilePage from './pages/ProfilePage'
 import { useAuthStore } from './store/useAuthStore.js'
+import { useThemeStore } from './store/useThemeStore.js'
 
 import {Loader} from "lucide-react"
 import { Toaster } from 'react-hot-toast'
@@ -17,6 +18,8 @@ import { Toaster } from 'react-hot-toast'
 const App = () => {
   const {authUser,checkAuth, isCheckingAuth} = useAuthStore();
 
+  const {theme} = useThemeStore();
+                        
   useEffect(()=>{
     checkAuth();
   },[checkAuth]);
@@ -32,7 +35,7 @@ const App = () => {
     
 
   return (
-    <div >
+    <div data-theme={theme}>
       {/* <Navbar/> → Renders the navigation bar on top (always visible). */}
       <Navbar/>
       {/* Handles page routing. */}
