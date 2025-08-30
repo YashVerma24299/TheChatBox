6fea9d530df4194f8e6836ce48c386abc7fbcd4d
Yash <ritikverma23@gmail.com>
2025-08-30 12:57:29 +0530

    Implemented signup with useAuthStore, added toaster, and built right-side UI

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -12,6 +12,7 @@ import ProfilePage from './pages/ProfilePage'
 import { useAuthStore } from './store/useAuthStore.js'
 
 import {Loader} from "lucide-react"
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   const {authUser,checkAuth, isCheckingAuth} = useAuthStore();
@@ -42,6 +43,8 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+
+      <Toaster/>
     </div>
   )
 }
