a26fe94ee9309504f88e0552de9dc78afb832bb4
Yash <ritikverma23@gmail.com>
2025-08-30 13:44:32 +0530

    Implement login and logout component with UI and functionality

--- Client/src/store/useAuthStore.js
+++ Client/src/store/useAuthStore.js
@@ -39,4 +39,38 @@ export const useAuthStore = create((set) => ({
           set({ isSigningUp: false });
       }
     },
+
+    // Login: Sends credentials → gets JWT + user data → saves user in store → shows toast → unlocks app.
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            // Sends a POST request to your backend (/api/auth/login) and data contains { email, password }.
+            const res = await axiosInstance.post("/auth/login", data);
+            // Backend verifies credentials:
+                // If correct → generates JWT token + sends back user object.
+                // If incorrect → sends error message.
+            set({ authUser: res.data });// Saves the logged-in user’s data and Now any component that uses authUser knows the user is logged in.
+            toast.success("Logged in successfully");
+        } catch (error) {
+            // shows the exact backend message.
+            toast.error(error.response.data.message);
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    }, 
+
+    // Logout: Sends logout request → clears cookie in backend → removes user from store → shows toast → app redirects to login.
+    logout: async () => {
+        try {
+            await axiosInstance.post("/auth/logout");
+            // Backend clears the JWT cookie (res.cookie("jwt", "", { maxAge: 0 })).
+            // This removes the session → user is no longer authenticated.
+            set({ authUser: null });
+            toast.success("Logged out successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+        }
+    },
+
+
 }))
